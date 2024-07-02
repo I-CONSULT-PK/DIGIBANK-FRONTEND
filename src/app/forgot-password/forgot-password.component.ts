292baa6b15479d648be20d88a102ef046c30f6d3
Mirza Uraib <mirzauraiboo7@gmail.com>
2024-07-02 14:33:47 +0500

    Work Status Summary For Mentioned Tasks: Sprint Number # 1: -------------------------------------------------------   Id    |          Name               |      Status   | ------------------------------------------------------- DIGI-4     Creation of Signup              Completed  | DIGI-5     Creation of Login               Completed  | DIGI-6     Creation of Frgt Username       Completed  | DIGI-7     Creation of Frgt Password       Completed  | DIGI-8     Creation of OTP                 Completed  | -------------------------------------------------------

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'forgot-password',
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-    constructor() { }
-    
+    constructor(private router: Router) { }
+    tittle: any = "Enter Your Credentials For Forget User Name";
     async ngOnInit() {
     }
     stepCounter: any = 1;
@@ -18,4 +19,8 @@ export class ForgotPasswordComponent implements OnInit {
     async StepDown() {
         this.stepCounter--;
     }
+    async BackLogin() {
+        this.router.navigate(["/login"]);
+    }
 }
+
